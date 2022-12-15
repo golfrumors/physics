9d8ff960de12042714ee1871fa58ed76da50696d
golfrumors <30129070+golfrumors@users.noreply.github.com>
2022-12-14 22:30:52 -0500

    initial commit

---
+++ build/CMakeFiles/physics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/physics.dir/src/main.cpp.o"
+  "CMakeFiles/physics.dir/src/main.cpp.o.d"
+  "physics"
+  "physics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/physics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
